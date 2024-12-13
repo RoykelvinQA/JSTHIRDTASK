98a78516a74df9307ddef01268ab3b49a6ccc47e
RoykelvinQA <adebiyitemitope88@gmail.com>
2024-12-13 09:20:35 -0800

    first commit

---
+++ Array.js
@@ -0,0 +1,25 @@
+const friend1 = "Bolaji"
+const friend2 = "Dele" 
+const friend3 = "Demola"
+const friend4 = "Femi"
+const friend5 = "Wasiu"
+
+const friend = [
+    "Bolaji",
+    "Dele",
+    "Demola",
+    "Femi",
+    "Wasiu"
+]
+
+const Bestfriend = [ "Bolaji", "Dele", "Demola", "Femi", "Wasiu" ]
+
+const Friend = new Array ("Bolaji", "Dele", "Demola", "Femi", "Wasiu")
+Bestfriend[4] = "Adio"
+
+console.log(Bestfriend)
+console.log(Bestfriend.toString())
+console.log(Bestfriend.length)
+console.log(Bestfriend[Bestfriend.length -2])
+
+console.log(Bestfriend[2]  +  ' and '  + Bestfriend [4])
--- Bigroy.js
+++ Bigroy.js
@@ -1,31 +1,49 @@
-let number = 100 % 5 
+// let number = 100 % 5 
 
-if(number == 0) {
-    console.log("even number")
+// if(number == 0) {
+//     console.log("even number")
 
+// }
+// else{
+//     console.log ("odd number")
+// }
+ 
+// let side1 = 50
+// let side2 = 40
+// let side3 = 30
+
+// if( side1 === side2 && side1 === side3){
+
+//     console.log("Equiliateral triangle")
+// }
+// else if ( side1 === side2 || side1 === side3 || side2 === side3 ){
+
+//     console.log("isosceles triangle")
+// }
+// else {
+//     console.log ("scalene triangle")
+// }
+   
+// for ( let number = 1 ; number <= 50 ; number ++) 
+// if(number % 2 === 1){
+
+//     console.log(number)
+// }
+
+
+let day = "Thursday"
+if (day === "Friday"){
+    console.log("TGIF")
+}
+else if ( day === "Sunday"){
+    console.log("Happy Sunday")
+}
+else if (day ==="Saturday"){
+    console.log(" It's a Weekend")
+}
+else if ( day === "Wednesday"){
+    console.log("It's Midweek")
 }
 else{
-    console.log ("odd number")
-}
- 
-let side1 = 50
-let side2 = 40
-let side3 = 30
-
-if( side1 === side2 && side1 === side3){
-
-    console.log("Equiliateral triangle")
-}
-else if ( side1 === side2 || side1 === side3 || side2 === side3 ){
-
-    console.log("isosceles triangle")
-}
-else {
-    console.log ("scalene triangle")
-}
-   
-for ( let number = 1 ; number <= 50 ; number ++) 
-if(number % 2 === 1){
-
-    console.log(number)
+    console.log(" Work your Ass Hard")
 }
